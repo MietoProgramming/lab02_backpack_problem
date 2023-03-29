c0c30223845f95cc38945b8b95a7e73671249a66
surteq <jasiekkrupinski@gmail.com>
2023-03-29 22:19:40 +0200

    JK changes

--- main2.py
+++ main2.py
@@ -58,24 +58,96 @@ def mutation(genom):
 
 def generateTable(items, genomstable):
     num_items = len(items)
-    table1 = np.zeros((populationNb + 3, num_items), dtype=int)
+    table = np.zeros((populationNb + 3, num_items), dtype=int)
 
-    # First table
     # First row: Numbers of items
-    table1[0] = np.arange(1, num_items+1)
+    table[0] = np.arange(1, num_items+1)
 
     # Second row: Weight for item
-    table1[1] = [item["weight"] for item in items]
+    table[1] = [item["weight"] for item in items]
 
     # Third row: Value for item
-    table1[2] = [item["value"] for item in items]
+    table[2] = [item["value"] for item in items]
 
     # Remaining rows: 0 or 1 depending on random generated list from genoms
     for i in range(3, populationNb+3):
         for j in range(num_items):
-            table1[i][j] = genomstable[i-3][j]
+            table[i][j] = genomstable[i-3][j]
 
-    return table1
+    return table
+
+
+def createPlot(genoms1, genoms2, best_values, avg_values, plotGenomValues, iterationsArr):
+    # Create a figure with a 3x2 grid of subplots
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+    plt.subplots_adjust(left=None, bottom=None, right=None,
+                        top=0.9, wspace=0.85, hspace=0.314)
+    plt.rcParams['axes.titlepad'] = 40
+
+    table1 = generateTable(items, genoms1)
+    table2 = generateTable(items, genoms2)
+    rows = ['Items Index', 'Weight', 'Value'] + \
+        ['Genoms ' + str(i) for i in range(1, 21)]
+
+    # TABLE 1
+    axs[0, 0].set_title("1st. Iteration Table")
+    axs[0, 0].axis('off')
+    table = axs[0, 0].table(cellText=table1, cellLoc='center',
+                            loc='center', rowLabels=rows)
+    # Format the first column
+    for i in range(0, 23):
+        cell = table[i, -1].get_text()
+        cell.set_fontweight('bold')
+    # Format the first three rows
+    for j in range(3):
+        for i in range(10):
+            cell = table[j, i].get_text()
+            cell.set_fontweight('bold')
+
+    # TABLE 2
+    axs[0, 1].set_title("2nd. Iteration Table")
+    axs[0, 1].axis('off')
+    table = axs[0, 1].table(cellText=table2, cellLoc='center',
+                            loc='center', rowLabels=rows)
+    # Format the first column
+    for i in range(0, 23):
+        cell = table[i, -1].get_text()
+        cell.set_fontweight('bold')
+    # Format the first three rows
+    for j in range(3):
+        for i in range(10):
+            cell = table[j, i].get_text()
+            cell.set_fontweight('bold')
+
+    # PLOT 3
+    axs[0, 2].axis('off')
+    axs[0, 2].set_visible(False)
+
+    axs[1, 2].axhline(y=max(best_values), xmin=0, xmax=0.95, color='gray',
+                      linestyle="dashed", linewidth=1, label=max(best_values))
+    axs[1, 2].plot(iterationsArr, best_values, label='Best')
+    axs[1, 2].plot(iterationsArr, avg_values, label='Average')
+    axs[1, 2].set_position([0.7, 0.35, 0.228, 0.343])
+    plt.xlabel('Generation')
+    plt.ylabel('Values')
+    plt.title('Evolution of genoms using Genetic Algorithm')
+    plt.legend()
+
+    # value from the first and second generation
+    plot1 = plotGenomValues[0]
+    plot2 = plotGenomValues[1]
+
+    # PIE 1
+    axs[1, 0].set_title("1st. Iteration Roulette")
+    axs[1, 0].pie(
+        plot1, labels=[f"{(genom/sum(plot1))*100:.2f}%" for genom in plot1], radius=1.5, frame=False)
+
+    # PIE 2
+    axs[1, 1].set_title("2nd. Iteration Roulette")
+    axs[1, 1].pie(
+        plot2, labels=[f"{(genom/sum(plot2))*100:.2f}%" for genom in plot2], radius=1.5, frame=False)
+
+    plt.show()
 
 
 def main():
@@ -104,81 +176,10 @@ def main():
         best_values.append(max(genomsValues))
         avg_values.append(sum(genomsValues) / len(genomsValues))
         plotGenomValues.append(genomsValues)
-        print(genoms2)
-        print("values")
-        print(genomsValues)
-        print('Weights')
-        print(genomsWeights)
-        
+
     iterationsArr = range(iterations)
-    # Create a figure with a 3x2 grid of subplots
-    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
-    plt.subplots_adjust(left=None, bottom=None, right=None,
-                        top=0.9, wspace=0.85, hspace=0.5)
-
-    table1 = generateTable(items, genoms1)
-    table2 = generateTable(items, genoms2)
-    
-    rows = ['Items Index', 'Weight', 'Value'] + \
-        ['Individual ' + str(i) for i in range(1, 21)]
-
-    # TABLE 1
-    # plt.rcParams['axes.titley'] = 1    # y is in axes-relative coordinates.
-    plt.rcParams['axes.titlepad'] = 30
-    axs[0, 0].set_title("Generating 1 Table")
-    axs[0, 0].axis('off')
-    table = axs[0, 0].table(cellText=table1, cellLoc='center',
-                            loc='center', rowLabels=rows)
-    # Format the first column
-    for i in range(0, 23):
-        cell = table[i, -1].get_text()
-        cell.set_fontweight('bold')
-    # Format the first three rows
-    for j in range(3):
-        for i in range(10):
-            cell = table[j, i].get_text()
-            cell.set_fontweight('bold')
-
-    # TABLE 2
-    axs[0, 1].set_title("Generating 2 Table")
-    axs[0, 1].axis('off')
-    table = axs[0, 1].table(cellText=table2, cellLoc='center',
-                            loc='center', rowLabels=rows)
-    # Format the first column
-    for i in range(0, 23):
-        cell = table[i, -1].get_text()
-        cell.set_fontweight('bold')
-    # Format the first three rows
-    for j in range(3):
-        for i in range(10):
-            cell = table[j, i].get_text()
-            cell.set_fontweight('bold')
-
-    # Create the third subplot by merging the last column of the first row
-    # with the entire second row
-    axs[0, 2].axis('off')
-    axs[0, 2].set_visible(False)
-
-    axs[1, 2].axhline(y=max(best_values), xmin=0, xmax=0.95, color='gray', linestyle="dashed", linewidth=1, label=max(best_values))
-    axs[1, 2].plot(iterationsArr, best_values, label='Best')
-    axs[1, 2].plot(iterationsArr, avg_values, label='Average')
-    axs[1, 2].set_position([0.7, 0.35, 0.228, 0.343])
-    plt.xlabel('Generation')
-    plt.ylabel('Values')
-    plt.title('Evolution of genoms using Genetic Algorithm')
-    plt.legend()
-
-    #wartość z pierwszej i drugiej generacji
-    plot1 = plotGenomValues[0]
-    plot2 = plotGenomValues[1]
-
-    # Create the fourth and fifth subplots in the second row
-    axs[1, 0].set_title("1 st. generation roulette")
-    axs[1, 0].pie(plot1, labels=[f"{(genom/sum(plot1))*100:.2f}%" for genom in plot1])
-    axs[1, 1].set_title("2 nd. generation roulette")
-    axs[1, 1].pie(plot2, labels=[f"{(genom/sum(plot2))*100:.2f}%" for genom in plot2])
-
-    plt.show()
+    createPlot(genoms1, genoms2, best_values, avg_values,
+               plotGenomValues, iterationsArr)
 
 
 main()
